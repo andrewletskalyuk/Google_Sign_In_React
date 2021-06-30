c51c754c6fa76ba7f3a5557b0e48a0195a8dfdb4
ALetskalyuk <andrewletskalyuk@gmail.com>
2021-06-30 10:04:29 +0300

    env

--- src/App.js
+++ src/App.js
@@ -1,62 +1,3 @@
-// import React, { Component } from 'react'
-// import './App.css'
-
-// class App extends Component {
-//   componentDidMount() {
-//     const _onInit = auth2 => {
-//       console.log('init OK', auth2)
-//     }
-//     const _onError = err => {
-//       console.log('error', err)
-//     }
-//     window.gapi.load('auth2', function() {
-//       window.gapi.auth2
-//         .init({ // не забудьте указать ваш ключ в .env
-//           client_id:
-//             '582846519895-2ee43128mpfa31il4u9gmdde4totebdf',
-//         })
-//         .then(_onInit, _onError)
-//     })
-//   }
-//   signIn = () => {
-//     const auth2 = window.gapi.auth2.getAuthInstance()
-//     auth2.signIn().then(googleUser => {
-
-//       // метод возвращает объект пользователя
-//       // где есть все необходимые нам поля
-//       const profile = googleUser.getBasicProfile()
-//       console.log('ID: ' + profile.getId()) // не посылайте подобную информацию напрямую, на ваш сервер!
-//       console.log('Full Name: ' + profile.getName())
-//       console.log('Given Name: ' + profile.getGivenName())
-//       console.log('Family Name: ' + profile.getFamilyName())
-//       console.log('Image URL: ' + profile.getImageUrl())
-//       console.log('Email: ' + profile.getEmail())
-
-//       // токен
-//       const id_token = googleUser.getAuthResponse().id_token
-//       console.log('ID Token: ' + id_token)
-//     })
-//   }
-//   signOut = () => {
-//     const auth2 = window.gapi.auth2.getAuthInstance()
-//     auth2.signOut().then(function() {
-//       console.log('User signed out.')
-//     })
-//   }
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <button onClick={this.signIn}>Log in</button>
-//           <button onClick={this.signOut}>Log out</button>
-//         </header>
-//       </div>
-//     )
-//   }
-// }
-
-// export default App
-
 import React, { Component } from "react";
 import { User } from "./components/User";
 import "./App.css";
@@ -107,16 +48,21 @@ class App extends Component {
     return (
       <div className="App">
         <div className="container">
-          <div className="row" style={{height:auto, backgroundColor:"pink"}}>
-            <header className=''>
+          <div
+            className="row"
+            style={{ height: auto, backgroundColor: "pink" }}
+          >
+            <header className="">
               {!name && <button onClick={this.signIn}>Log in</button>}
               {!!name && <button onClick={this.signOut}>Log out</button>}
               {!!name && <User name={name} imgUrl={imgUrl} />}
             </header>
+            <div className="container">
+              {process.env.NODE_ENV === "development"
+                ? process.env.REACT_APP_DEV_MODE
+                : process.env.REACT_APP_PRO_MODE}
+            </div>
           </div>
-          {/* <div className='row'>
-            <Form/>
-          </div> */}
         </div>
       </div>
     );
